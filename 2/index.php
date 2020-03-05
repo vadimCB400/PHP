ac637f42e33d5e2b02c97a278aa36b08f192d7d5
vadimCB400 <43133984+vadimCB400@users.noreply.github.com>
2020-03-05 15:41:00 +0300

    debug

--- 2/index.php
+++ 2/index.php
@@ -1,45 +1,44 @@
 <?php
-post();
+include "start.html";
 function post(){
-    include "start.html";
-    $firstText = $_REQUEST["text"];
     $finishText = "";
+    $firstText = $_REQUEST["arguments"];
+    $firstText = str_split($firstText, 1);
     foreach (generator($firstText) as $elem) {
         $finishText .= $elem;
+        print $finishText;
     }
-    print "; Итоговое слово: " . $finishText;
 }
 
 function generator($firstText){
     $counter = 0;
-    for ($i = 0; $i < strlen($firstText); $i++) {
+    for ($i = 0; $i = strlen($firstText); $i++) {
         switch ($firstText[$i]) {
             case "h" :
                 $firstText[$i] = "4";
                 yield $firstText[$i];
-                $counter++;
                 break;
+                $counter++;
             case "i" :
                 $firstText[$i] = "1";
                 yield $firstText[$i];
-                $counter++;
                 break;
+                $counter++;
             case "e" :
                 $firstText[$i] = "3";
                 yield $firstText[$i];
-                $counter++;
                 break;
+                $counter++;
             case "o" :
                 $firstText[$i] = "0";
                 yield $firstText[$i];
-                $counter++;
                 break;
+                $counter++;
             default :
-                yield $firstText[$i];
+                $firstText[$i];
         }
     }
-    print "Перестановок: " . $counter;
+    return $counter;
 }
-
 
 ?>
